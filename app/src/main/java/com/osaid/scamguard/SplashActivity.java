99862ff431c8d4feef41241c1a656dc134c8ae26
Osaid2993 <osaidbahadry@gmail.com>
2026-04-17 02:51:31 +1000

    Add animated splash screen with staggered fade-in and scale

--- app/src/main/java/com/osaid/scamguard/SplashActivity.java
+++ app/src/main/java/com/osaid/scamguard/SplashActivity.java
@@ -1,13 +1,10 @@
 package com.osaid.scamguard;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.animation.DecelerateInterpolator;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,32 +21,30 @@ public class SplashActivity extends AppCompatActivity {
         TextView title = findViewById(R.id.splashTitle);
         TextView tagline = findViewById(R.id.splashTagline);
 
-        ObjectAnimator shieldFade = ObjectAnimator.ofFloat(shield, View.ALPHA, 0f, 1f);
-        ObjectAnimator shieldScaleX = ObjectAnimator.ofFloat(shield, View.SCALE_X, 0.7f, 1f);
-        ObjectAnimator shieldScaleY = ObjectAnimator.ofFloat(shield, View.SCALE_Y, 0.7f, 1f);
+        shield.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(600)
+                .setInterpolator(new DecelerateInterpolator())
+                .start();
 
-        AnimatorSet shieldSet = new AnimatorSet();
-        shieldSet.playTogether(shieldFade, shieldScaleX, shieldScaleY);
-        shieldSet.setDuration(600);
-        shieldSet.setInterpolator(new DecelerateInterpolator());
+        title.animate()
+                .alpha(1f)
+                .setStartDelay(300)
+                .setDuration(500)
+                .start();
 
-        ObjectAnimator titleFade = ObjectAnimator.ofFloat(title, View.ALPHA, 0f, 1f);
-        titleFade.setDuration(500);
-        titleFade.setStartDelay(300);
-
-        ObjectAnimator taglineFade = ObjectAnimator.ofFloat(tagline, View.ALPHA, 0f, 1f);
-        taglineFade.setDuration(500);
-        taglineFade.setStartDelay(500);
-
-        AnimatorSet fullSet = new AnimatorSet();
-        fullSet.playTogether(shieldSet, titleFade, taglineFade);
-        fullSet.start();
+        tagline.animate()
+                .alpha(1f)
+                .setStartDelay(500)
+                .setDuration(500)
+                .start();
 
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        }, 1700);
+        }, 1600);
     }
 }
